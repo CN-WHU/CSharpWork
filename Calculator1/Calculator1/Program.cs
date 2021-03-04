94838f65307d9f5cb80711f5ae5a82b60b55c016
CN-WHU <2817977590@qq.com>
2021-03-04 23:29:30 +0800

    two files of CSharp homework

---
+++ Calculator1/Calculator1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+            string Symbol;
+
+            Console.WriteLine("请输入第一个数：");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数：");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("请从'+','-','*','/'运算符中选择一个输入：");
+            Symbol = Console.ReadLine();
+
+            switch (Symbol)
+            {
+                case "+":result = num1 + num2;
+                    Console.WriteLine("结果是{0}", result);
+                    break;
+                case "-":result = num1 - num2;
+                    Console.WriteLine("结果是{0}", result);
+                    break;
+                case "*":result = num1 * num2;
+                    Console.WriteLine("结果是{0}", result);
+                    break;
+                case "/":result = num1 / num2;
+                    Console.WriteLine("结果是{0}", result);
+                    break;
+                default: Console.WriteLine("请输入有效的运算符");
+                    break;
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculator2/Calculator2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string snum1, snum2, sresult, Symbol;
+            double num1, num2, result = 0;
+
+            snum1 = textBox1.Text;
+            snum2 = textBox2.Text;
+            Symbol = comboBox1.Text;
+            num1 = Convert.ToDouble(snum1);
+            num2 = Convert.ToDouble(snum2);
+
+            switch (Symbol)
+            {
+                case "+":result = num1 + num2;break;
+                case "-":result = num1 - num2;break;
+                case "*":result = num1 * num2;break;
+                case "/":result = num1 / num2;break;
+                default:MessageBox.Show("请选择数学符号！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    break;
+            }
+            sresult = result.ToString();
+            textBox3.Text = sresult;
+
+
+
+        }
+    }
+}

70b9baf7e110f7c0c6d4b55c7aaeee63caed2130
CN-WHU <54747108+CN-WHU@users.noreply.github.com>
2021-06-27 22:47:04 +0800

    Add files via upload

---
+++ homework7/homework7/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework9
+{
+    public partial class Form1 : Form
+    {
+        public Crawler myCrawler;
+        public Thread thread;
+        bool crawl;
+        public Form1()
+        {
+            InitializeComponent();
+            myCrawler = new Crawler();
+            crawl = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crawl = true;
+            thread = new Thread(myCrawler.Start);
+            thread.Start(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "暂停爬取")
+            {
+                crawl = false;
+                thread.Suspend();
+                button2.Text = "继续爬取";
+            }
+            else if(button2.Text=="继续爬取")
+            {
+                crawl = true;
+                thread.Resume();
+                button2.Text = "暂停爬取";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            richTextBox1.Text = myCrawler.ToString(DownloadedPages<string,true>);
+        }
+    }
+}
